090a486ede7961a5d8e374ece6e237739daecd30
cmedinilla <cris.medinill@gmail.com>
2019-12-30 23:14:14 +0000

    Deploying to master from  ea53f508b6374e8e14504a8d351a420fd386e643

---
+++ precache-manifest.8952bbdaa4424ab347f679aa4065ebc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea25589eee18658ac686d2d2f72fc47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d077c50720595c4cb01d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "678332677917b5007f5d",
+    "url": "/static/js/2.37c12b73.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.37c12b73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d077c50720595c4cb01d",
+    "url": "/static/js/main.977793b2.chunk.js"
+  },
+  {
+    "revision": "2309e5884056e9048b41",
+    "url": "/static/js/runtime-main.3c6428e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
